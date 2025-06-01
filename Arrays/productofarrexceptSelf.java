042867d20b1c61b5d681d678317dde7c52163f56
unknown <tanishach19@gmail.com>
2025-06-01 14:26:33 +0530

    Add product of array except self solution in Arrays folder

---
+++ Arrays/productofarrexceptSelf.java
@@ -0,0 +1,25 @@
+//Link:
+//TC:O(N)
+//SC:O(N)
+// Status: Solved ✅ | Date: 2025-06-01
+
+class productofarrexeptSelf{
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        prefix[0] = 1;
+        suffix[n-1] = 1;
+        for(int i=1; i<n; i++){
+            prefix[i] = nums[i-1]*prefix[i-1];
+        }
+        for(int i=n-2; i>=0; i--){
+            suffix[i] = nums[i+1]*suffix[i+1];
+        }
+        for(int i=0; i<n; i++){
+            res[i] = prefix[i]*suffix[i];
+        }
+        return res;
+    }
+}
