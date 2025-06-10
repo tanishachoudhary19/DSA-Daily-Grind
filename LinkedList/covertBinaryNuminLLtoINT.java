dd5ce37b15a204fe650ae21973a492b584106b84
unknown <tanishach19@gmail.com>
2025-06-10 15:16:10 +0530

    Added Convert Binary Number in Linked List to Integer question in Linked List folder

---
+++ LinkedList/covertBinaryNuminLLtoINT.java
@@ -0,0 +1,17 @@
+//Link:https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
+//TC:O(N)
+//SC:O(N)
+// Status: Solved ✅ | Date: 2025-06-10
+
+// class covertBinaryNuminLLtoINT{
+//     public int getDecimalValue(ListNode head) {
+//         ListNode temp = head;
+//         int result = 0;
+//         while(temp!= null){
+//             result = result*2+temp.val;
+//             temp = temp.next;
+//         }
+//         return result;
+//     }
+
+// }
