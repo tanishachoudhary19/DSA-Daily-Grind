c3f3e5754c13cf99bed2c57ab2787855ccd95edc
unknown <tanishach19@gmail.com>
2025-06-23 12:38:15 +0530

    Added Sort Even and Odd indices Independently in Arrays folder

---
+++ Arrays/SortEvenAndOddIndicesIndependently.java
@@ -0,0 +1,27 @@
+
+import java.util.Arrays;
+
+class SortEvenAndOddIndicesIndependently{
+    public int[] sortEvenOdd(int[] nums){
+        int n = nums.length;
+        int evencount = (n+1)/2;
+        int oddcount = n/2;
+        int[] even = new int[evencount];
+        int[] odd = new int[oddcount];
+        for(int i=0; i<n; i++){
+            if(i % 2 == 0) even[i/2] = nums[i];
+            else odd[i/2] = nums[i];
+        }
+        Arrays.sort(even);
+        Arrays.sort(odd);
+        int evenindex = 0;
+        int oddindex = oddcount-1;
+        for(int i=0; i<n; i+=2){
+            nums[i] = even[evenindex++];
+        }
+        for(int i=1; i<n; i+=2){
+            nums[i] = odd[oddindex--];
+        }
+        return nums;
+    }
+}
