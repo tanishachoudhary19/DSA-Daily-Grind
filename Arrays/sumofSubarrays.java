1e16bea203164c49be48706138498ef000a1ac6b
unknown <tanishach19@gmail.com>
2025-07-23 18:45:48 +0530

    Added Sum of Subarrays

---
+++ Arrays/sumofSubarrays.java
@@ -0,0 +1,15 @@
+//Link:https://www.geeksforgeeks.org/problems/sum-of-subarrays2229/1
+//tc:O(n)
+//sc:O(1)
+//Status: Solved ✅ | Date: 2025-07-22
+
+class sumofSubarrays{
+    public int subarraySum(int[] arr) {
+        int n = arr.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i]*(i+1)*(n-i);
+        }
+        return sum;
+    }
+}
