e2a5e62178efa549bfd05de2c0a9addec555435f
unknown <tanishach19@gmail.com>
2025-08-05 18:56:54 +0530

    Added Reverse a Stack

---
+++ Recursion/ReverseStack.java
@@ -0,0 +1,24 @@
+//Link:https://www.geeksforgeeks.org/problems/reverse-a-stack/1
+//tc:O(n2)
+//sc: O(n)
+//Status: Solved ✅ | Date: 2025-08-5
+
+import java.util.Stack;
+class ReverseStack{
+    static void insert(Stack<Integer> St, int temp){
+        if(st.isEmpty()){
+            st.push(temp);
+            return;
+        }
+        int top = st.pop();
+        insert(st,temp);
+        st.push(top);
+    }
+    static void reverse(Stack<Integer> st){
+        if(st.isEmpty()) return;
+        int temp = st.peek();
+        st.pop();
+        reverse(st);
+        insert(st,temp);
+    }
+}
