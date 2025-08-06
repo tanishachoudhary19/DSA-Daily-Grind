bbf60b7b8163ae1b149b04930e15379e02355dcb
unknown <tanishach19@gmail.com>
2025-08-06 19:27:30 +0530

    Added Kth Symbol in Grammar

---
+++ Recursion/kthSymbolinGrammar.java
@@ -0,0 +1,10 @@
+class kthSymbolinGrammar{
+        public int kthGrammar(int n, int k) {
+        if(n == 1 || k == 1) return 0;
+        int size = (int)Math.pow(2,n-1);
+        int mid = size/2;
+        if(k > mid) return 1 - kthGrammar(n-1, k-mid);
+        else return kthGrammar(n-1, k);
+
+    }
+}
