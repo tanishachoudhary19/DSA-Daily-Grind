7e9d156acaacc1b7973635e33f052c2f67317c6e
unknown <tanishach19@gmail.com>
2025-08-06 23:21:38 +0530

    Middle of Linked List

--- LinkedList/middleofLL.java
+++ LinkedList/middleofLL.java
@@ -3,15 +3,15 @@
 //sc: O(n)
 //Status: Solved ✅ | Date: 2025-08-6
 
-import java.util.LinkedList;
-class middleofLL{
-     public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-}
+// import java.util.LinkedList;
+// class middleofLL{
+//      public ListNode middleNode(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while(fast != null && fast.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         return slow;
+//     }
+// }
