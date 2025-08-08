4e1c32d612130ebd495d1c5e58d230b02dda476c
unknown <tanishach19@gmail.com>
2025-08-08 15:06:21 +0530

    Added Longest Prefix Suffix

--- Strings/longestPrefixSuffix.java
+++ Strings/longestPrefixSuffix.java
@@ -3,27 +3,27 @@
 //sc:O(n)
 // Status: Solved ✅ | Date: 2025-08-08
 
-class longestPrefixSuffix{
-    int getLPSLength(String s) {
-        // code here
-        int n = s.length();
-        int[] lps = new int[n];
-        lps[0] = 0;
-        int i=1; 
-        int length = 0;
-        while(i < s.length()){
-            if(s.charAt(i) == s.charAt(length)){
-                length++;
-                lps[i] = length;
-                i++;
-            }else{
-                if(length != 0) length = lps[length-1];
-                else{
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-        return lps[n-1];
-    }
-}
+// class longestPrefixSuffix{
+//     int getLPSLength(String s) {
+//         // code here
+//         int n = s.length();
+//         int[] lps = new int[n];
+//         lps[0] = 0;
+//         int i=1; 
+//         int length = 0;
+//         while(i < s.length()){
+//             if(s.charAt(i) == s.charAt(length)){
+//                 length++;
+//                 lps[i] = length;
+//                 i++;
+//             }else{
+//                 if(length != 0) length = lps[length-1];
+//                 else{
+//                     lps[i] = 0;
+//                     i++;
+//                 }
+//             }
+//         }
+//         return lps[n-1];
+//     }
+// }
