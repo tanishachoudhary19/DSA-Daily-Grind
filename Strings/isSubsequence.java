78dd19fe7797713197b60f730976d43c06e624e6
unknown <tanishach19@gmail.com>
2025-08-08 12:41:03 +0530

    Addeded is Subsequence

--- Strings/isSubsequence.java
+++ Strings/isSubsequence.java
@@ -3,20 +3,20 @@
 //sc:O(1)
 // Status: Solved ✅ | Date: 2025-08-08
 
-class isSubsequence{
-    public boolean isSubsequence(String s, String t) {
-        int i=0,j=0;
-        while(i < s.length() && j < t.length()){
-            char c = s.charAt(i);
-            char ch = t.charAt(j);
-            if(c == ch){
-                i++;
-                j++;
-            }else{
-                j++;
-            }
-        }
-        if(i < s.length()) return false;
-        return true;
-    }
-}
+// class isSubsequence{
+//     public boolean isSubsequence(String s, String t) {
+//         int i=0,j=0;
+//         while(i < s.length() && j < t.length()){
+//             char c = s.charAt(i);
+//             char ch = t.charAt(j);
+//             if(c == ch){
+//                 i++;
+//                 j++;
+//             }else{
+//                 j++;
+//             }
+//         }
+//         if(i < s.length()) return false;
+//         return true;
+//     }
+// }
