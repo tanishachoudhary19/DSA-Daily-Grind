1ca3ee00679ce58b51a875f935aa249bc1443fca
unknown <tanishach19@gmail.com>
2025-08-09 18:10:23 +0530

    Added Permutation with Spaces

--- Recursion/permutationwithSpaces.java
+++ Recursion/permutationwithSpaces.java
@@ -3,20 +3,20 @@
 //sc:O(2^N.X)
 // Status: Solved ✅ | Date: 2025-08-09
 
-import java.util.*;
-class permutationwithSpaces{
-    private void helper(int index, String s, String output,ArrayList<String> res){
-        if(index == s.length()){
-            res.add(output);
-            return;
-        }
-        helper(1,s,output+s.charAt(index),res);
-        helper(1,s,output+" "+s.charAt(index),res);
-    }
-    ArrayList<String> permutation(String s){
-        ArrayList<String> res = new ArrayList<>();
-        helper(1,s,""+s.charAt(0),res);
-        Collections.sort(res);
-        return res;
-    }
-}
+// import java.util.*;
+// class permutationwithSpaces{
+//     private void helper(int index, String s, String output,ArrayList<String> res){
+//         if(index == s.length()){
+//             res.add(output);
+//             return;
+//         }
+//         helper(1,s,output+s.charAt(index),res);
+//         helper(1,s,output+" "+s.charAt(index),res);
+//     }
+//     ArrayList<String> permutation(String s){
+//         ArrayList<String> res = new ArrayList<>();
+//         helper(1,s,""+s.charAt(0),res);
+//         Collections.sort(res);
+//         return res;
+//     }
+// }
